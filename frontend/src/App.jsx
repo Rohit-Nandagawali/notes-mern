fd73d16d5898a7f50ef56d5424c7119e6761b1b5
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2023-09-05 01:21:09 +0530

    fa

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+
+const Card = ({ email, name }) => {
+  return (
+    <div className="card">
+      <p className='name'>{name}</p>
+      <p className='email'>{email}</p>
+    </div>
+  );
+}
+// hello
+
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const getUser = async () => {
+    let result = await fetch('http://localhost:5000/list');
+    result = await result.json();
+    setUsers(result)
+  }
+
+  return (
+    <div className="App">
+      <h1>Data Fetched From API</h1>
+      {
+        users.map((item, index) => (
+
+          <Card key={index} name={item.name} email={item.email} />
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
